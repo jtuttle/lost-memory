d8293040598491065d3b935bbf785924c58c2f5d
John Tuttle <jtuttle.develops@gmail.com>
2021-01-31 12:59:39 -0500

    Fix newspaper, move player

--- Lost Memory/Assets/Scripts/PlayerRaycast.cs
+++ Lost Memory/Assets/Scripts/PlayerRaycast.cs
@@ -34,6 +34,7 @@ public class PlayerRaycast : MonoBehaviour
         if(hit)
         {
             target = hitInfo.collider.gameObject.GetComponent<StoryObject>();
+            Debug.Log(target);
         }
 
         // remove outline from previous target
