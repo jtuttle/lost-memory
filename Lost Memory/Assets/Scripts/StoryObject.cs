a4117414e87ceab7e618e227defe028430a89b86
John Tuttle <jtuttle.develops@gmail.com>
2021-01-31 13:29:44 -0500

    Remove comment

--- Lost Memory/Assets/Scripts/StoryObject.cs
+++ Lost Memory/Assets/Scripts/StoryObject.cs
@@ -13,10 +13,6 @@ public class StoryObject : MonoBehaviour
     {
         _outlines = GetComponentsInChildren<cakeslice.Outline>();
         SetOutline(false);
-
-        // assumes first child is object model w/ Outline script
-        //_outline = gameObject.transform.GetChild(0).GetComponent<cakeslice.Outline>();
-        //_outline.enabled = false;
     }
 
     public void SetOutline(bool value)
